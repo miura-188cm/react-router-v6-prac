7fbe6b852b01eba670c76e160620379c5eb42756
T.Miura <mtaiga0213@icloud.com>
2026-02-17 23:53:06 +0900

    feat: error page の実装
    なんでこんな形になるのかがわからない
    useRouteError()がunknown 返すのが意味不明普通に厳密に決めてもらえませんか

---
+++ src/error-page.tsx
@@ -0,0 +1,20 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error("error ", error);
+
+  let errorMessage = ''
+  if (isRouteErrorResponse(error)) {
+    errorMessage = error.statusText
+  }
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{errorMessage}</i>
+      </p>
+    </div>
+  );
+}
