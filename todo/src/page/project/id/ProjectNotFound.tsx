70c062eabd1c245501dfccc548eb1f43eddfc4f6
T.Miura <mtaiga0213@icloud.com>
2026-02-21 15:47:06 +0900

    notfound の実装

---
+++ todo/src/page/project/id/ProjectNotFound.tsx
@@ -0,0 +1,8 @@
+export default function ProjectNotFound() {
+  return (
+    <div>
+      <h2>Project Not Found</h2>
+      <p>指定されたプロジェクトは見つかりませんでした。</p>
+    </div>
+  )
+}
--- todo/src/state.ts
+++ todo/src/state.ts
@@ -1,4 +1,5 @@
 import { atom } from "jotai"
+import { atomFamily } from "jotai/utils"
 import type { Todo, Project } from "./type"
 const todos: Todo[] = [
   {
@@ -10,6 +11,9 @@ const todos: Todo[] = [
   }
 ]
 export const todoAtom = atom<Todo[]>(todos)
+export const todoByProjectIdAtom = atomFamily((projectId: string) =>
+  atom(get => get(todoAtom).filter(todo => todo.projectId === projectId))
+)
 
 const projects: Project[] = [
   {
@@ -19,3 +23,13 @@ const projects: Project[] = [
   }
 ]
 export const projectAtom = atom<Project[]>(projects)
+
+export const projectByIdAtom = atomFamily((projectId: string) =>
+  atom<Project>(get => {
+    const project = get(projectAtom).find(p => p.id === projectId)
+    if (!project) {
+      throw new Error('Project not found')
+    }
+    return project
+  })
+)
