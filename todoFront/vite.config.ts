ad2a0c203507a39719a98ea20e17bcb32bea612f
T.Miura <mtaiga0213@icloud.com>
2026-02-22 20:51:19 +0900

    backend に接続できなかったのはdocker内で localhost を指定していたからだった docker の localhost とhost の localhost は見ているものが違う

--- todoFront/vite.config.ts
+++ todoFront/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/projects': 'http://localhost:3000',
+      '/projects': 'http://host.docker.internal:3000',
     }
   }
 })
