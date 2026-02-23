0825774addf5fecb3a38ed3979c6fbc5041cb901
T.Miura <mtaiga0213@icloud.com>
2026-02-23 13:22:16 +0900

    formDataをactionで取得してsubmit

--- todoFront/src/page/project/id/CreateTodo.tsx
+++ todoFront/src/page/project/id/CreateTodo.tsx
@@ -1,24 +1,26 @@
-import { Form } from "react-router-dom";
+import { Form, type ActionFunctionArgs } from "react-router-dom";
 import { apiFetch } from '../../../lib/apiFetch'
 
-export const createTodoAction = async ({params}) => {
-  try {
-    const pro = '093dc44d-ddcc-44b9-baaa-16534ff7f357'
-    await apiFetch(`/projects/${pro}/todos`, {
-      method: 'POST',
-      body: JSON.stringify({ title: 'sample', due: new Date() })
-    })
-  } catch (error) {
-    console.log('error:loader:', error)
-  }
+export const createTodoAction = async ({ request, params }: ActionFunctionArgs) => {
+
+  const { projectId } = params
+
+  const formData = await request.formData()
+  const titleField = formData.get('title')
+  const dueField = formData.get('due')
+  await apiFetch(`/projects/${projectId}/todos`, {
+    method: 'POST',
+    body: JSON.stringify({ title: titleField, due: dueField })
+  })
+
   return null
 }
 
 export default function CreateTodo() {
   return (
     <Form method="post">
-      <input type="text" name="title"/>
-      <input type="date" name="due"/>
+      <input type="text" name="title" />
+      <input type="date" name="due" />
       <button type="submit">追加</button>
     </Form>
   )
